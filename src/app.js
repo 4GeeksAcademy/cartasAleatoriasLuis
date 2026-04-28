466d70aeb68ab1f349af9ae18cc4721a6a282762
Luis Timaure <luistimaure1204@gmail.com>
2026-04-28 01:45:53 +0000

    Cambios de Generador de cartas Ajuste de html, css y js para generador de cartas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateCard();
 };
+
+function generateCard() {
+  const suits = ["heart", "diamond", "spade", "club"];
+  const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  const icons = {
+    heart: "♥",
+    diamond: "♦",
+    spade: "♠",
+    club: "♣"
+  };
+
+  let randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  let randomValue = values[Math.floor(Math.random() * values.length)];
+  
+  document.querySelector("#idCard").className = "card " + randomSuit;
+  document.querySelector(".top").innerHTML = icons[randomSuit];
+  document.querySelector(".bottom").innerHTML = icons[randomSuit];
+   document.querySelector(".number").innerHTML = randomValue;
+}
+
